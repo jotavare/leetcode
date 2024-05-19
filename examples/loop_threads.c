951f67326b319433581dd8d91bd4c3191b25491b
João Oliveira <38820667+jotavare@users.noreply.github.com>
2024-05-19 23:00:00 +0100

    feat: add the threading examples
    Threads, joins, mutexes, trylock, condition variables, and fork against pthread_create.

---
+++ examples/loop_threads.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+int mails = 0;
+pthread_mutex_t mutex;
+
+void *rotine()
+{
+    for (int i = 0; i < 10000000; i++)
+    {
+        pthread_mutex_lock(&mutex);
+        mails++;
+        pthread_mutex_unlock(&mutex);
+    }
+}
+
+int main(int ac, char **av)
+{
+    int nThreads = 4;
+    pthread_t th[nThreads];
+    pthread_mutex_init(&mutex, NULL);
+
+    for (int i = 0; i < nThreads; i++)
+    {
+        if (pthread_create(&th[i], NULL, &rotine, NULL) != 0)
+        {
+            perror("Failed to create thread.");
+            return 1;
+        }
+        printf("Thread %d has started.\n", i);
+    }
+
+    for (int i = 0; i < nThreads; i++)
+    {
+        if (pthread_join(th[i], NULL) != 0)
+        {
+            perror("Failed to join thread.");
+            return 2;
+        }
+        printf("Thread %d has finished execution.\n", i);
+    }
+
+    pthread_mutex_destroy(&mutex);
+    printf("Number of mails: %d\n", mails);
+    return 0;
+}
---
+++ examples/pthread_mutex.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+int mails = 0;
+pthread_mutex_t mutex;
+
+void *rotine()
+{
+    for (int i = 0; i < 10000000; i++)
+    {
+        pthread_mutex_lock(&mutex);
+        mails++;
+        pthread_mutex_unlock(&mutex);
+    }
+}
+
+int main(int ac, char **av)
+{
+    pthread_t t1, t2, t3, t4;
+    pthread_mutex_init(&mutex, NULL);
+
+    if (pthread_create(&t1, NULL, &rotine, NULL) != 0)
+        return 1;
+    if (pthread_create(&t2, NULL, &rotine, NULL) != 0)
+        return 2;
+    if (pthread_create(&t3, NULL, &rotine, NULL) != 0)
+        return 3;
+    if (pthread_create(&t4, NULL, &rotine, NULL) != 0)
+        return 4;
+    if (pthread_join(t1, NULL) != 0)
+        return 5;
+    if (pthread_join(t2, NULL) != 0)
+        return 6;
+    if (pthread_join(t3, NULL) != 0)
+        return 7;
+    if (pthread_join(t4, NULL) != 0)
+        return 8;
+
+    pthread_mutex_destroy(&mutex);
+
+    printf("Number of mails: %d\n", mails);
+
+    return 0;
+}
